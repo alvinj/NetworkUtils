cd9271a408f0d3e6754d2258756142fd7edac0f3
Alvin Alexander <devdaily@gmail.com>
2012-08-27 13:46:17 -0600

    Wow, those first methods were horrible; rewrote them.

--- src/main/scala/NetworkUtils.scala
+++ src/main/scala/NetworkUtils.scala
@@ -14,61 +14,34 @@ object NetworkUtils {
 
   /**
    * Returns the text (content) from a REST URL as a String.
-   * Returns a blank String if there was a problem.
-   * This function will also throw exceptions if there are problems trying
-   * to connect to the url.
-   *
-   * @param url A complete URL, such as "http://foo.com/bar"
+   * This method does not use a timeout, so it probably shouldn't be used in
+   * production projects.
+   * Usage: val content = getRestContent("http://localhost:8080/getStocks")
    */
-  def getRestContent(url: String): String = {
-    val httpClient = new DefaultHttpClient
-    val httpResponse = httpClient.execute(new HttpGet(url))
-    val entity = httpResponse.getEntity
-    var content = ""
-    if (entity != null) {
-      val inputStream = entity.getContent
-      content = io.Source.fromInputStream(inputStream).getLines.mkString
-      inputStream.close
-    }
-    httpClient.getConnectionManager.shutdown
-    return content
-  }
-
+  @throws(classOf[java.io.IOException])
+  def getRestContent(url: String) = io.Source.fromURL(url).mkString
 
   /**
    * Returns the text (content) from a REST URL as a String.
-   * Returns a blank String if there was a problem.
-   * This function will also throw exceptions if there are problems trying
-   * to connect to the url.
-   * 
-   * @param url A complete URL, such as "http://foo.com/bar"
-   * @param connectionTimeout The connection timeout, in ms.
-   * @param socketTimeout The socket timeout, in ms.
+   * Inspired by http://matthewkwong.blogspot.com/2009/09/scala-scalaiosource-fromurl-blockshangs.html
+   * and http://www.devdaily.com/blog/post/java/how-open-url-read-contents-httpurl-connection-java
+   * The timeout value is used for both the setConnectTimeout and setReadTimeout Connection methods.
+   * As a practical matter, if a service is down, the method will return in timeout ms.
+   * Usage: val content = getRestContent("http://localhost:8080/getStocks", 5000)
    */
-  def getRestContent(url: String, connectionTimeout: Int, socketTimeout: Int): String = {
-    val httpClient = buildHttpClient(connectionTimeout, socketTimeout)
-    val httpResponse = httpClient.execute(new HttpGet(url))
-    val entity = httpResponse.getEntity
-    var content = ""
-    if (entity != null) {
-      val inputStream = entity.getContent
-      content = io.Source.fromInputStream(inputStream).getLines.mkString
-      inputStream.close
-    }
-    httpClient.getConnectionManager.shutdown
-    return content
+  @throws(classOf[java.io.IOException])
+  @throws(classOf[java.net.SocketTimeoutException])
+  def getRestContent(url: String, timeout: Int):String = {
+    import java.net.{URL, HttpURLConnection}
+    val connection = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
+    connection.setConnectTimeout(timeout)
+    connection.setReadTimeout(timeout)
+    connection.setRequestMethod("GET")
+    val inputStream = connection.getInputStream
+    val content = io.Source.fromInputStream(inputStream).mkString
+    inputStream.close
+    content
   }
-  
-  private def buildHttpClient(connectionTimeout: Int, socketTimeout: Int): DefaultHttpClient = {
-    val httpClient = new DefaultHttpClient
-    val httpParams = httpClient.getParams
-    HttpConnectionParams.setConnectionTimeout(httpParams, connectionTimeout)
-    HttpConnectionParams.setSoTimeout(httpParams, socketTimeout)
-    httpClient.setParams(httpParams)
-    httpClient
-  }
-
-
 
 }
 
